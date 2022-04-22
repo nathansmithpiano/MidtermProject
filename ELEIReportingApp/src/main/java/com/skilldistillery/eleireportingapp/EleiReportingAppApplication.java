b19b5c72f6eecea10a4075058b56b7e4d8925a00
NathanSmith <nathansmithpiano@gmail.com>
2022-04-22 09:59:26 -0600

    Create Spring Boot MVC project, add JPA project dependency

---
+++ ELEIReportingApp/src/main/java/com/skilldistillery/eleireportingapp/EleiReportingAppApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.eleireportingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleiReportingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleiReportingAppApplication.class, args);
+	}
+
+}
---
+++ ELEIReportingApp/src/test/java/com/skilldistillery/eleireportingapp/EleiReportingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.eleireportingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleiReportingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
