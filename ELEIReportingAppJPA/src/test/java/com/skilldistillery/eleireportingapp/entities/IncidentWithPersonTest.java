efad0d199c753f82b8bdf0c0261eb03ee4e11748
aslusaf <aslusaf@live.com>
2022-04-22 16:47:21 -0600

    IncidentWithPerson Test Stubbed Out

---
+++ ELEIReportingAppJPA/src/test/java/com/skilldistillery/eleireportingapp/entities/IncidentWithPersonTest.java
@@ -0,0 +1,65 @@
+package com.skilldistillery.eleireportingapp.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class IncidentWithPersonTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private IncidentWithPerson incidentWithPerson;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	    emf = Persistence.createEntityManagerFactory("ELEIReportingAppJPA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	    emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	    em = emf.createEntityManager();
+	    incidentWithPerson = em.find(IncidentWithPerson.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	    em.close();
+	    incidentWithPerson = null;
+	}
+	
+	@Test
+	@DisplayName("test basic IncidentWithPerson mappings")
+	void test1() {
+		
+//		SELECT * FROM incident_with_person iwp WHERE (iwp.incident_id = 1 && iwp.person_id = 4);
+//		+-----------+-------------+-----------------+-------------+-------------+-------+-------------------------------------------------+
+//		| person_id | incident_id | suspected_crime | age_minimum | age_maximum | notes | description                                     |
+//		+-----------+-------------+-----------------+-------------+-------------+-------+-------------------------------------------------+
+//		|         4 |           1 | Shoplift        |          20 |          25 | NULL  | Suspect of shoplifting power tools from Walmart |
+//		+-----------+-------------+-----------------+-------------+-------------+-------+-------------------------------------------------+
+		
+		
+		assertNotNull(incidentWithPerson);
+		assertEquals("Shoplift", incidentWithPerson.getSuspectedCrime());
+		assertEquals(20, incidentWithPerson.getAgeMinimum());
+		assertEquals(25, incidentWithPerson.getAgeMaximum());
+		assertEquals(null, incidentWithPerson.getNotes());
+		assertEquals("Suspect of shoplifting power tools from Walmart", incidentWithPerson.getDescription());
+		
+	}
+
+}
