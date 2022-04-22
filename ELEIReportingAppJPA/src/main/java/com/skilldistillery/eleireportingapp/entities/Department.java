93552f3c09abbce0f0329fd1fcde8495585b4153
aslusaf <aslusaf@live.com>
2022-04-22 14:51:20 -0600

    Completed Department

--- ELEIReportingAppJPA/src/main/java/com/skilldistillery/eleireportingapp/entities/Department.java
+++ ELEIReportingAppJPA/src/main/java/com/skilldistillery/eleireportingapp/entities/Department.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.eleireportingapp.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,9 +9,52 @@ import javax.persistence.Id;
 
 @Entity
 public class Department {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+
+	private String name;
+
+	public Department() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
 
 }
