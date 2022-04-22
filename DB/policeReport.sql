0aaeaed2424c2c6fe528c6fb800a25e2e405fd2e
Dustin Jennings <dustinejennings@gmail.com>
2022-04-22 14:47:11 -0600

    db updated with one row of information to test

--- DB/policeReport.sql
+++ DB/policeReport.sql
@@ -16,29 +16,42 @@ CREATE SCHEMA IF NOT EXISTS `eleidb` DEFAULT CHARACTER SET utf8 ;
 USE `eleidb` ;
 
 -- -----------------------------------------------------
+-- Table `ethnicity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ethnicity` ;
+
+CREATE TABLE IF NOT EXISTS `ethnicity` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `person`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `person` ;
 
 CREATE TABLE IF NOT EXISTS `person` (
   `id` INT NOT NULL COMMENT 'User isA Person\\n',
-  `type` VARCHAR(45) NOT NULL,
   `first_name` VARCHAR(45) NULL DEFAULT NULL,
   `middle_name` VARCHAR(45) NULL,
   `last_name` VARCHAR(45) NULL,
   `title` VARCHAR(45) NULL,
   `birthdate` DATE NULL,
-  `age` VARCHAR(45) NOT NULL,
   `gender` VARCHAR(45) NOT NULL,
-  `ethnicity` VARCHAR(45) NOT NULL,
   `address_id` VARCHAR(45) NULL DEFAULT NULL,
-  `supervisor_id` INT NULL,
-  `officer_id` INT NULL,
   `flag` TINYINT NULL,
   `description` TEXT NULL,
   `notes` TEXT NULL,
+  `ethnicity_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `officer_id_UNIQUE` (`officer_id` ASC))
+  INDEX `fk_person_ethnicity1_idx` (`ethnicity_id` ASC),
+  CONSTRAINT `fk_person_ethnicity1`
+    FOREIGN KEY (`ethnicity_id`)
+    REFERENCES `ethnicity` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -50,11 +63,10 @@ DROP TABLE IF EXISTS `user` ;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL,
   `person_id` INT NOT NULL,
-  `type` VARCHAR(45) NULL DEFAULT NULL,
+  `permission_level` VARCHAR(45) NULL DEFAULT NULL,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `active` TINYINT NULL,
-  `role` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_User_Person1_idx` (`person_id` ASC),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
@@ -67,49 +79,29 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `department`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `department` ;
-
-CREATE TABLE IF NOT EXISTS `department` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `address_id` VARCHAR(45) NULL,
-  `person_id` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `address`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `address` ;
 
 CREATE TABLE IF NOT EXISTS `address` (
   `id` INT NOT NULL,
-  `type` VARCHAR(45) NULL DEFAULT NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
   `street1` VARCHAR(255) NULL DEFAULT NULL,
   `street2` VARCHAR(255) NULL DEFAULT NULL,
   `city` VARCHAR(45) NULL DEFAULT NULL,
   `state_code` VARCHAR(2) NULL DEFAULT NULL,
   `zip` INT(5) UNSIGNED NULL DEFAULT NULL,
-  `department_id` INT NOT NULL,
   `flag` TINYINT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_address_department1_idx` (`department_id` ASC),
-  CONSTRAINT `fk_address_department1`
-    FOREIGN KEY (`department_id`)
-    REFERENCES `department` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `person_has_address`
+-- Table `person_address`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `person_has_address` ;
+DROP TABLE IF EXISTS `person_address` ;
 
-CREATE TABLE IF NOT EXISTS `person_has_address` (
+CREATE TABLE IF NOT EXISTS `person_address` (
   `person_id` INT NOT NULL,
   `address_id` INT NOT NULL,
   PRIMARY KEY (`person_id`, `address_id`),
@@ -129,13 +121,13 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `case`
+-- Table `case_file`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `case` ;
+DROP TABLE IF EXISTS `case_file` ;
 
-CREATE TABLE IF NOT EXISTS `case` (
+CREATE TABLE IF NOT EXISTS `case_file` (
   `id` INT NOT NULL,
-  `type` VARCHAR(45) NULL,
+  `suspected_crime` VARCHAR(45) NULL,
   `description` TEXT NULL,
   `flag` TINYINT NULL,
   PRIMARY KEY (`id`))
@@ -149,13 +141,13 @@ DROP TABLE IF EXISTS `incident` ;
 
 CREATE TABLE IF NOT EXISTS `incident` (
   `id` INT NOT NULL,
-  `type` VARCHAR(45) NULL DEFAULT NULL,
+  `reason_for_contact` VARCHAR(45) NULL DEFAULT NULL,
   `location` VARCHAR(45) NULL DEFAULT NULL,
   `address_id` INT NOT NULL,
   `case_id` INT NOT NULL,
   `description` TEXT NULL,
   `flag` TINYINT NULL,
-  `datetime` DATETIME NULL,
+  `incident_date` DATETIME NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_incident_address1_idx` (`address_id` ASC),
   INDEX `fk_incident_case1_idx` (`case_id` ASC),
@@ -166,21 +158,25 @@ CREATE TABLE IF NOT EXISTS `incident` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_incident_case1`
     FOREIGN KEY (`case_id`)
-    REFERENCES `case` (`id`)
+    REFERENCES `case_file` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `person_has_incident`
+-- Table `incident_with_person`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `person_has_incident` ;
+DROP TABLE IF EXISTS `incident_with_person` ;
 
-CREATE TABLE IF NOT EXISTS `person_has_incident` (
+CREATE TABLE IF NOT EXISTS `incident_with_person` (
   `person_id` INT NOT NULL,
   `incident_id` INT NOT NULL,
-  `type` VARCHAR(45) NULL,
+  `suspected_crime` VARCHAR(45) NULL,
+  `age_minimum` INT NOT NULL,
+  `age_maximum` INT NOT NULL,
+  `notes` TEXT NULL,
+  `description` VARCHAR(45) NULL,
   PRIMARY KEY (`person_id`, `incident_id`),
   INDEX `fk_person_has_incident_incident1_idx` (`incident_id` ASC),
   INDEX `fk_person_has_incident_person1_idx` (`person_id` ASC),
@@ -198,24 +194,19 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `department_has_person`
+-- Table `department`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `department_has_person` ;
+DROP TABLE IF EXISTS `department` ;
 
-CREATE TABLE IF NOT EXISTS `department_has_person` (
-  `department_id` INT NOT NULL,
-  `person_id` INT NOT NULL,
-  PRIMARY KEY (`department_id`, `person_id`),
-  INDEX `fk_department_has_person_person1_idx` (`person_id` ASC),
-  INDEX `fk_department_has_person_department1_idx` (`department_id` ASC),
-  CONSTRAINT `fk_department_has_person_department1`
-    FOREIGN KEY (`department_id`)
-    REFERENCES `department` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_department_has_person_person1`
-    FOREIGN KEY (`person_id`)
-    REFERENCES `person` (`id`)
+CREATE TABLE IF NOT EXISTS `department` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `address_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_department_address1_idx` (`address_id` ASC),
+  CONSTRAINT `fk_department_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -230,18 +221,18 @@ CREATE TABLE IF NOT EXISTS `note` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `created` TIMESTAMP NULL,
   `updated` TIMESTAMP NULL,
-  `text` TEXT NULL,
+  `content` TEXT NULL,
   `user_id` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `incident_has_note`
+-- Table `incident_note`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `incident_has_note` ;
+DROP TABLE IF EXISTS `incident_note` ;
 
-CREATE TABLE IF NOT EXISTS `incident_has_note` (
+CREATE TABLE IF NOT EXISTS `incident_note` (
   `incident_id` INT NOT NULL,
   `note_id` INT NOT NULL,
   PRIMARY KEY (`incident_id`, `note_id`),
@@ -261,11 +252,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `case_has_note`
+-- Table `case_file_note`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `case_has_note` ;
+DROP TABLE IF EXISTS `case_file_note` ;
 
-CREATE TABLE IF NOT EXISTS `case_has_note` (
+CREATE TABLE IF NOT EXISTS `case_file_note` (
   `case_id` INT NOT NULL,
   `note_id` INT NOT NULL,
   PRIMARY KEY (`case_id`, `note_id`),
@@ -273,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `case_has_note` (
   INDEX `fk_case_has_note_case1_idx` (`case_id` ASC),
   CONSTRAINT `fk_case_has_note_case1`
     FOREIGN KEY (`case_id`)
-    REFERENCES `case` (`id`)
+    REFERENCES `case_file` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_case_has_note_note1`
@@ -285,11 +276,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `person_has_note`
+-- Table `person_note`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `person_has_note` ;
+DROP TABLE IF EXISTS `person_note` ;
 
-CREATE TABLE IF NOT EXISTS `person_has_note` (
+CREATE TABLE IF NOT EXISTS `person_note` (
   `person_id` INT NOT NULL,
   `note_id` INT NOT NULL,
   PRIMARY KEY (`person_id`, `note_id`),
@@ -307,6 +298,56 @@ CREATE TABLE IF NOT EXISTS `person_has_note` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `officer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `officer` ;
+
+CREATE TABLE IF NOT EXISTS `officer` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `person_id` INT NOT NULL,
+  `supervisor_id` INT NOT NULL,
+  `badge` VARCHAR(45) NULL,
+  `image_url` VARCHAR(2000) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_officer_officer1_idx` (`supervisor_id` ASC),
+  CONSTRAINT `fk_officer_person1`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `person` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_officer_officer1`
+    FOREIGN KEY (`supervisor_id`)
+    REFERENCES `officer` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `department_employee`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `department_employee` ;
+
+CREATE TABLE IF NOT EXISTS `department_employee` (
+  `officer_id` INT NOT NULL,
+  `department_id` INT NOT NULL,
+  PRIMARY KEY (`officer_id`, `department_id`),
+  INDEX `fk_officer_has_department_department1_idx` (`department_id` ASC),
+  INDEX `fk_officer_has_department_officer1_idx` (`officer_id` ASC),
+  CONSTRAINT `fk_officer_has_department_officer1`
+    FOREIGN KEY (`officer_id`)
+    REFERENCES `officer` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_officer_has_department_department1`
+    FOREIGN KEY (`department_id`)
+    REFERENCES `department` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 SET SQL_MODE = '';
 DROP USER IF EXISTS officeruser@localhost;
 SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -319,11 +360,21 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `ethnicity`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `ethnicity` (`id`, `name`) VALUES (1, 'WHITE');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `person`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `eleidb`;
-INSERT INTO `person` (`id`, `type`, `first_name`, `middle_name`, `last_name`, `title`, `birthdate`, `age`, `gender`, `ethnicity`, `address_id`, `supervisor_id`, `officer_id`, `flag`, `description`, `notes`) VALUES (1, '1', 'test', 'test', 'test', 'admin', NULL, '22', 'male', 'white', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `person` (`id`, `first_name`, `middle_name`, `last_name`, `title`, `birthdate`, `gender`, `address_id`, `flag`, `description`, `notes`, `ethnicity_id`) VALUES (1, 'FIRSTNAME', 'MIDDLENAME', 'LASTNAME', 'admin', '9999-12-31', 'male', 'ADDRESSID', NULL, 'DESCRIPTION', 'NOTES', 1);
 
 COMMIT;
 
@@ -333,7 +384,127 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `eleidb`;
-INSERT INTO `user` (`id`, `person_id`, `type`, `username`, `password`, `active`, `role`) VALUES (1, 1, 'admin', 'admin', 'admin', 1, 'role_admin');
+INSERT INTO `user` (`id`, `person_id`, `permission_level`, `username`, `password`, `active`) VALUES (1, 1, 'admin', 'admin', 'admin', 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `address`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `address` (`id`, `description`, `street1`, `street2`, `city`, `state_code`, `zip`, `flag`) VALUES (1, 'DESCRIPTION', 'STREET1', 'STREET2', 'CITY', 'CO', ZIP, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `person_address`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `person_address` (`person_id`, `address_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `case_file`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `case_file` (`id`, `suspected_crime`, `description`, `flag`) VALUES (1, 'SUSPECTED CRIME', 'DESCRIPTION', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `incident`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `incident` (`id`, `reason_for_contact`, `location`, `address_id`, `case_id`, `description`, `flag`, `incident_date`) VALUES (1, 'REASONFORCONTACT', 'LOCATION', 1, 1, 'DESCRIPTION', NULL, '9999-12-31');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `incident_with_person`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `incident_with_person` (`person_id`, `incident_id`, `suspected_crime`, `age_minimum`, `age_maximum`, `notes`, `description`) VALUES (1, 1, 'SUSPECTED CRIME', 1, 99, 'NOTES', 'DESCRIPTION');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `department`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `department` (`id`, `address_id`, `name`) VALUES (1, 1, 'SKILL DISTILLERY PD');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `note`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `note` (`id`, `created`, `updated`, `content`, `user_id`) VALUES (1, NULL, NULL, 'CONTENT', '1');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `incident_note`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `incident_note` (`incident_id`, `note_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `case_file_note`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `case_file_note` (`case_id`, `note_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `person_note`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `person_note` (`person_id`, `note_id`) VALUES (1, 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `officer`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `officer` (`id`, `person_id`, `supervisor_id`, `badge`, `image_url`) VALUES (1, 1, 1, '2201', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `department_employee`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `eleidb`;
+INSERT INTO `department_employee` (`officer_id`, `department_id`) VALUES (1, 1);
 
 COMMIT;
 
