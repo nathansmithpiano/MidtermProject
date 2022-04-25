f9cc73d400434598979ff0ddbbeb515e9dd057dd
aslusaf <aslusaf@live.com>
2022-04-25 12:14:06 -0600

    UserDAOImpls completed w/tests

--- ELEIReportingApp/src/main/java/com/skilldistillery/eleireportingapp/data/UserDAO.java
+++ ELEIReportingApp/src/main/java/com/skilldistillery/eleireportingapp/data/UserDAO.java
@@ -7,7 +7,7 @@ import com.skilldistillery.eleireportingapp.entities.User;
 public interface UserDAO {
 	
 	public User findById(int id);
-	public User findByPersonId(int personId);
+//	public User findByPersonId(int personId);
 	public List<User> findByPermissionLevel(String level);
 	public List<User> findByStatus(boolean active);
 	public User findByUserName(String userName);
