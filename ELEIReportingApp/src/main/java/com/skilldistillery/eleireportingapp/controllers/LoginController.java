9feb84b79b80d22b05b7e0def9ccc5d9d3dbd25a
Dustin Jennings <dustinejennings@gmail.com>
2022-04-26 09:08:56 -0600

    update to login controller

--- ELEIReportingApp/src/main/java/com/skilldistillery/eleireportingapp/controllers/LoginController.java
+++ ELEIReportingApp/src/main/java/com/skilldistillery/eleireportingapp/controllers/LoginController.java
@@ -1,16 +1,27 @@
 package com.skilldistillery.eleireportingapp.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.eleireportingapp.entities.User;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping(path = {"login.do" } )
-	public String startLogin(Model model) {
-		
-		return "login";
+	@RequestMapping("login.do")
+	public ModelAndView displayLogin(HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("user", new User());
+		if (session.getAttribute("user") != null) {
+			mv.setViewName("redirect:index.do");
+		} else {
+			mv.addObject("userCommandObject", new User());
+			mv.setViewName("login");
+		}
+		return mv;
 	}
 
 }
